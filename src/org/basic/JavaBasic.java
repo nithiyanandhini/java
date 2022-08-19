97a6e9375a1140f6d2b31b229bdbb582c6d0880a
nithiyanandhini <nithiyadass@gmail.com>
2022-08-19 11:13:43 +0530

    new code

---
+++ src/org/basic/JavaBasic.java
@@ -0,0 +1,13 @@
+package org.basic;
+
+public class JavaBasic {
+	
+	private void trainer() {
+System.out.println("shiva");
+	}
+	public static void main(String[] args) {
+		JavaBasic j=new JavaBasic();
+		j.trainer();
+	}
+
+}
